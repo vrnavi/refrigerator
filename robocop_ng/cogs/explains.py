9a8a302e7677849c368afa5f8d1ad67f44707815
wistlyr <lumi@whistler.page>
2022-10-02 13:58:42 -0400

    add command to tell people off for journal

--- robocop_ng/cogs/explains.py
+++ robocop_ng/cogs/explains.py
@@ -16,6 +16,11 @@ class Explains(Cog):
     async def dumpster(self, ctx):
         """Explains Dumpster."""
         await ctx.send("Dumpster has not, is not, and will never be a core part of this server. It is not coming back.")
+        
+    @commands.command(hidden=True, aliases=["embeds", "howpostembeds"])
+    async def journal(self, ctx):
+        """Explains Strange Journal."""
+        await ctx.send("To post embeds, react to messages, or speak in voice channels, you need the Strange Journal role.\nTo learn how to get this role, read #controls. **Do __not__ \"spoonfeed\" any user the command for it.**")
 
 async def setup(bot):
     await bot.add_cog(Explains(bot))
