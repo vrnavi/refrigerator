7186a848f0a824f0d14f01a68066d8e68165868f
wistlyr <lumi@whistler.page>
2022-10-03 00:36:33 -0400

    dishwasher.

--- robocop_ng/cogs/basic.py
+++ robocop_ng/cogs/basic.py
@@ -30,7 +30,7 @@ class Basic(Cog):
     async def install(self, ctx):
         """[U] Teaches you how to install a Dishwasher."""
         await ctx.send(
-            f"Here's how to install a dishwasher:\nhttps://www.whirlpool.com/blog/kitchen/how-to-install-a-dishwasher.html\n\nWhile you're at it, consider protecting your dishwasher:\nhttps://www.2-10.com/homeowners-warranty/dishwasher/\n\nRemember, the more time you spend with your dishwasher instead of the kitchen sink, __the better__."
+            f"Here's how to install a dishwasher:\n<https://www.whirlpool.com/blog/kitchen/how-to-install-a-dishwasher.html>\n\nWhile you're at it, consider protecting your dishwasher:\n<https://www.2-10.com/homeowners-warranty/dishwasher/>\n\nRemember, the more time you spend with your dishwasher instead of the kitchen sink, __the better__."
         )
 
     @commands.cooldown(1, 10, type=commands.BucketType.user)
