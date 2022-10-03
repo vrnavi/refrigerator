ec637641f2e8a4efb758b136ef0febf79d1754df
wistlyr <lumi@whistler.page>
2022-10-03 00:59:21 -0400

    move help

--- robocop_ng/cogs/basic.py
+++ robocop_ng/cogs/basic.py
@@ -34,10 +34,10 @@ class Basic(Cog):
         )
         
     @commands.command()
-        async def help(self, ctx, *args: str):
-            """Shows this message."""
-            await commands.bot._default_help_command(ctx, *args, dm_help=True)
-            await ctx.send("📬")
+    async def help(self, ctx, *args: str):
+        """Shows this message."""
+        await commands.bot._default_help_command(ctx, *args, dm_help=True)
+        await ctx.send("📬")
 
     @commands.cooldown(1, 10, type=commands.BucketType.user)
     @commands.command(name="hex")
