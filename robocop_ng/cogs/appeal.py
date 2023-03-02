459b3ec5530c1f26e6c3b02518ba81729c3fa185
lapis <lapis@starrypoint.me>
2023-03-02 01:15:30 -0500

    staff count.

--- robocop_ng/cogs/appeal.py
+++ robocop_ng/cogs/appeal.py
@@ -27,7 +27,7 @@ class Appeal(Cog):
             await message.add_reaction("❎")
             await message.add_reaction("✳️")
             appealthread = await message.create_thread(name=f"{message.embeds[0].fields[1].value[:-5]}'s Appeal", reason="Automatic Appeal Thread Generating by Dishwasher.")
-            await appealthread.send(content=f"**Voting has MOVED to the appeal's post itself!**\nVote using reactions. Use this thread for discussion.\n✅ = Yes\n❎ = No\n✳️ = Abstain\n\nUntil it can be coded to automatically appear here, use `pws logs {message.embeds[0].fields[2].value}`.\nRemember to post ban context if available (ban record, modmail logs, etc.).")
+            await appealthread.send(content=f"**Voting has MOVED to the appeal's post itself!**\nVote using reactions. Use this thread for discussion.\n`✅ = Yes`\n`❎ = No`\n`✳️ = Abstain`\n\nUntil it can be coded to automatically appear here, use `pws logs {message.embeds[0].fields[2].value}`.\nRemember to post ban context if available (ban record, modmail logs, etc.).\n\nThere are currently `{len(message.guild.get_role(259199371361517569).members)}` Staff members at this time.\nVoting should end once one option reaches `{int(len(message.guild.get_role(259199371361517569).members)/2//1+1)}` votes.")
                 
 async def setup(bot):
     await bot.add_cog(Appeal(bot))
