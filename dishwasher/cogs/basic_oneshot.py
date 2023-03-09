a95317b17cf2021c815e69725020c062946b11f8
lapis <lapis@starrypoint.me>
2023-03-09 14:24:16 -0500

    first staff list attempt

--- dishwasher/cogs/basic_oneshot.py
+++ dishwasher/cogs/basic_oneshot.py
@@ -21,6 +21,59 @@ class BasicOneShot(Cog):
     async def ren(self, ctx):
         """[U] What does Dishwasher think about ren?"""
         await ctx.send("HELP! HELP! HELP!")
+        
+    @commands.guild_only()
+    @commands.command()
+    async def staff(self, ctx):
+        """[U] Shows currently active staff."""
+        staff_role = ctx.guild.get_role(config.staff_role_id)
+        embed = discord.Embed(
+            color=staff_role.color, title="🛠️ Staff List", description=f"Voting requirement is `{int(len(staff_role.members)/2//1+1)}`.", timestamp=datetime.datetime.now()
+        )
+        embed.set_footer(text="Dishwasher")
+        online = []
+        away = []
+        dnd = []
+        offline = []
+        mobile = []
+        for m in staff_role.members:
+            u = f"{m.mention}"
+            if m.is_on_mobile():
+                u = f"{m.mention} 📱"
+            if m.raw_status == "online":
+                online.append(u)
+            elif m.raw_status == "offline":
+                offline.append(u)
+            elif m.raw_status == "dnd":
+                dnd.append(u)
+            elif m.raw_status == "idle":
+                away.append(u)
+        onlinelist = ",".join(online)
+        offlinelist = ",".join(offline)
+        awaylist = ",".join(away)
+        dndlist = ",".join(dnd)
+        embed.add_field(
+            name="🟢 Online [`{len(onlinelist}}`/`{len(staff_role.members}`]",
+            value=f"{onlinelist}",
+            inline=False
+        )
+        embed.add_field(
+            name="🟡 Idle [`{len(awaylist}}`/`{len(staff_role.members}`]",
+            value=f"{awaylist}",
+            inline=False
+        )
+        embed.add_field(
+            name="🔴 Do Not Disturb [`{len(dndlist}}`/`{len(staff_role.members}`]",
+            value=f"{dndlist}",
+            inline=False
+        )
+        embed.add_field(
+            name="⚫ Offline [`{len(offlinelist}}`/`{len(staff_role.members}`]",
+            value=f"{offlinelist}",
+            inline=False
+        )
+        ctx.reply(embed=embed, mention_author=False)
+        
 
 async def setup(bot):
     await bot.add_cog(BasicOneShot(bot))
