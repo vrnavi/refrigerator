e3138c9031f2a0be1084cd82580fb83b80572f96
lapis <lapis@starrypoint.me>
2023-04-11 03:11:13 -0400

    keep dishtimers from filling with expired timestamps

--- dishwasher/helpers/dishtimer.py
+++ dishwasher/helpers/dishtimer.py
@@ -33,5 +33,9 @@ def delete_job(timestamp, job_type, job_name):
     ctab = get_crontab()
 
     del ctab[job_type][timestamp][job_name]
+    
+    # smh, not checking for empty timestamps. Smells like bloat!
+    if not ctab[job_type][timestamp]:
+        del ctab[job_type][timestamp]
 
     set_crontab(json.dumps(ctab))
