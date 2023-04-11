0a732ec00735f37ba42c638a21baa141980f23d3
lapis <lapis@starrypoint.me>
2023-04-11 19:58:09 -0400

    auto twitter links

--- dishwasher/cogs/messagescan.py
+++ dishwasher/cogs/messagescan.py
@@ -14,6 +14,10 @@ class Messagescan(Cog):
             r"https://(?:canary\.|ptb\.)?discord\.com/channels/[0-9]+/[0-9]+/[0-9]+",
             re.IGNORECASE,
         )
+        self.twitterlink_re = re.compile(
+            r"https://twitter\.com/[A-z0-9]+/status/[0-9]+",
+            re.IGNORECASE,
+        )
         self.prevmessages = {}
 
     @commands.guild_only()
@@ -47,36 +51,44 @@ class Messagescan(Cog):
         await self.bot.wait_until_ready()
 
         msglinks = self.link_re.findall(message.content)
-
-        if not msglinks:
+        twitterlinks = self.twitterlink_re.findall(message.content)
+        if not msglinks or twitterlinks:
             return
+        tlinks = None
+        embeds = None
 
-        embeds = []
-        for m in msglinks:
-            components = m.split("/")
-            guildid = int(components[4])
-            channelid = int(components[5])
-            msgid = int(components[6])
+        if twitterlinks:
+            tlinks = []
+            for t in twitterlinks:
+                tlinks.append(t[:8] + "vx" + t[8:])                
 
-            rcvguild = self.bot.get_guild(guildid)
-            rcvchannel = rcvguild.get_channel_or_thread(channelid)
-            rcvmessage = await rcvchannel.fetch_message(msgid)
+        if msglinks:
+            embeds = []
+            for m in msglinks:
+                components = m.split("/")
+                guildid = int(components[4])
+                channelid = int(components[5])
+                msgid = int(components[6])
 
-            # Prepare embed msg
-            embed = discord.Embed(
-                color=rcvmessage.author.color,
-                description=f"{rcvmessage.content}",
-                timestamp=rcvmessage.created_at,
-            )
-            embed.set_footer(
-                text=f"Quoted by {message.author.name}#{message.author.discriminator}"
-            )
-            embed.set_author(
-                name=f"💬 {rcvmessage.author.name}#{rcvmessage.author.discriminator} said in #{rcvmessage.channel.name}...",
-                icon_url=f"{rcvmessage.author.display_avatar.url}",
-            )
-            embeds.append(embed)
-        await message.reply(embeds=embeds, mention_author=False)
+                rcvguild = self.bot.get_guild(guildid)
+                rcvchannel = rcvguild.get_channel_or_thread(channelid)
+                rcvmessage = await rcvchannel.fetch_message(msgid)
+
+                # Prepare embed msg
+                embed = discord.Embed(
+                    color=rcvmessage.author.color,
+                    description=f"{rcvmessage.content}",
+                    timestamp=rcvmessage.created_at,
+                )
+                embed.set_footer(
+                    text=f"Quoted by {message.author.name}#{message.author.discriminator}"
+                )
+                embed.set_author(
+                    name=f"💬 {rcvmessage.author.name}#{rcvmessage.author.discriminator} said in #{rcvmessage.channel.name}...",
+                    icon_url=f"{rcvmessage.author.display_avatar.url}",
+                )
+                embeds.append(embed)
+        await message.reply(content=tlinks, embeds=embeds, mention_author=False)
 
     @Cog.listener()
     async def on_message_delete(self, message):
