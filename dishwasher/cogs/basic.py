2301c3581bfee04144841865f977024e37cbd66c
lapis <lapis@starrypoint.me>
2023-04-13 02:55:04 -0400

    info role

--- dishwasher/cogs/basic.py
+++ dishwasher/cogs/basic.py
@@ -113,7 +113,8 @@ class Basic(Cog):
     async def help(self, ctx):
         """Posts a help command."""
         await ctx.reply(
-            "[Press F1 For] HELP\nhttps://0ccu.lt/dishwasher/commands/", mention_author=False
+            "[Press F1 For] HELP\nhttps://0ccu.lt/dishwasher/commands/",
+            mention_author=False,
         )
 
     @commands.command(aliases=["showcolor"])
@@ -206,36 +207,25 @@ class Basic(Cog):
             await poll.add_reaction(poll_emoji[n])
 
     @commands.guild_only()
-    @commands.command()
+    @commands.group(invoke_without_command=True)
     async def info(self, ctx, target: discord.User = None):
         """[S] Gets full user info."""
         if target == None:
             target = ctx.author
 
-        if target.bot:
-            isbot = " [BOT]"
-        else:
-            isbot = ""
-
-        if ctx.guild.get_member(target.id) is None:
-            # Memberless code.
-            color = discord.Color.lighter_gray()
-            usertype = "user"
-            nickname = ""
-        else:
-            # Member code.
-            target = ctx.guild.get_member(target.id)
-            color = target.color
-            usertype = "member"
-            nickname = f"\n**Nickname:** `{target.nick}`"
+        nickname = (
+            f"\n**Nickname:** `{target.nick}`"
+            if ctx.guild.get_member(target.id)
+            else ""
+        )
 
         embed = discord.Embed(
-            color=color,
-            title=f"Statistics for {usertype} @{target}{isbot}",
+            color=target.color,
+            title=f"Statistics for {'user' if ctx.guild.get_member(target.id) else ''} @{target}{' [BOT]' if target.bot else ''}",
             description=f"**ID:** `{target.id}`{nickname}",
             timestamp=datetime.now(),
         )
-        embed.set_footer(text="Dishwasher")
+        embed.set_footer(text=self.bot.user.name, icon_url=self.bot.user.display_avatar)
         embed.set_author(name=f"{target}", icon_url=f"{target.display_avatar.url}")
         embed.set_thumbnail(url=f"{target.display_avatar.url}")
         embed.add_field(
@@ -243,7 +233,7 @@ class Basic(Cog):
             value=f"<t:{target.created_at.astimezone().strftime('%s')}:f>\n<t:{target.created_at.astimezone().strftime('%s')}:R>",
             inline=True,
         )
-        if ctx.guild.get_member(target.id) is not None:
+        if ctx.guild.get_member(target.id):
             embed.add_field(
                 name="⏱️ Account joined:",
                 value=f"<t:{target.joined_at.astimezone().strftime('%s')}:f>\n<t:{target.joined_at.astimezone().strftime('%s')}:R>",
@@ -254,24 +244,21 @@ class Basic(Cog):
                 value=f"`{sorted(ctx.guild.members, key=lambda v: v.joined_at).index(target)+1}` of `{len(ctx.guild.members)}`",
                 inline=True,
             )
-            emoji = ""
-            details = ""
-            name = ""
             try:
                 if target.activity.emoji is not None:
                     emoji = f"{target.activity.emoji} "
             except:
-                pass
+                emoji = ""
             try:
                 if target.activity.details is not None:
                     details = f"\n{target.activity.details}"
             except:
-                pass
+                details = ""
             try:
                 if target.activity.name is not None:
                     name = f"{target.activity.name}"
             except:
-                pass
+                name = ""
             embed.add_field(
                 name="💭 Status:", value=f"{emoji}{name}{details}", inline=False
             )
@@ -284,6 +271,38 @@ class Basic(Cog):
             embed.add_field(name=f"🎨 Roles:", value=f"{rolelist}", inline=False)
 
         await ctx.reply(embed=embed, mention_author=False)
+
+    @info.command()
+    async def role(self, ctx, role: discord.Role = None):
+        """[S] Gets full role info."""
+        if role == None:
+            role = ctx.guild.default_role
+
+        embed = discord.Embed(
+            color=role.color,
+            title=f"Statistics for role @{role}{' [MANAGED]' if role.managed else ''}",
+            description=f"**ID:** `{role.id}`\n**Color:** `{str(role.color)}`",
+            timestamp=datetime.now(),
+        )
+        embed.set_footer(text=self.bot.user.name, icon_url=self.bot.user.display_avatar)
+        embed.set_author(name=f"{role.guild.name}", icon_url=f"{role.guild.icon.url}")
+        embed.set_thumbnail(url=(role.icon.url if role.icon else None))
+        embed.add_field(
+            name="⏰ Role created:",
+            value=f"<t:{role.created_at.astimezone().strftime('%s')}:f>\n<t:{role.created_at.astimezone().strftime('%s')}:R>",
+            inline=True,
+        )
+        embed.add_field(
+            name="👥 Role members:",
+            value=f"`{len(role.members)}`",
+            inline=True,
+        )
+        embed.add_field(
+            name="🚩 Role flags:",
+            value=f"**Hoisted:** {str(role.hoist)}\n**Mentionable:** {str(role.mentionable)}",
+            inline=True,
+        )
+        ctx.reply(embed=embed, mention_author=False)
 
 
 async def setup(bot):
