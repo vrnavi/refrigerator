6a689b4245387a763470920eed5528e017f6e7b1
lapis <lapis@starrypoint.me>
2023-04-18 23:12:04 -0400

    i hate discord i hate discord 4

--- dishwasher/cogs/messagescan.py
+++ dishwasher/cogs/messagescan.py
@@ -181,9 +181,9 @@ class Messagescan(Cog):
                 embeds.append(embed)
         reply = await message.reply(content=tlinks, embeds=embeds, mention_author=False)
         # Discord SUCKS!!
-        await asyncio.sleep(1)
-        while message.embeds:
-            await message.edit(suppress=True)
+        while not message.embeds:
+            asyncio.sleep(0.1)
+        await message.edit(suppress=True)
         def deletecheck(m):
             return m.id == message.id
 
