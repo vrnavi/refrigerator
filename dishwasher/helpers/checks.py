02ee882b83afedb3ff8217656ef410ebf67f7a9a
vrnavi <lapis@starrypoint.me>
2023-06-09 01:34:34 -0400

    anyone with manage guild should be considered as staff

--- dishwasher/helpers/checks.py
+++ dishwasher/helpers/checks.py
@@ -1,19 +1,19 @@
 import config
+from sv_config import get_config
 
 
 def check_if_staff(ctx):
     if ctx.author.id in config.bot_managers:
         return True
-    if (
-        not ctx.guild
-        or ctx.guild.id not in config.guild_configs
-        or "staff" not in config.guild_configs[ctx.guild.id]
-        or "staff_role" not in config.guild_configs[ctx.guild.id]["staff"]
-    ):
+    if not ctx.guild:
         return False
     return any(
-        r.id == config.guild_configs[ctx.guild.id]["staff"]["staff_role"]
-        for r in ctx.author.roles
+        any(
+            r.id == get_config(ctx.guild.id, "staff", "staff_role")
+            for r in ctx.author.roles
+        ),
+        any(m == ctx.author.id for m in config.bot_managers),
+        ctx.author.guild_permissions.manage_guild,
     )
 
 
