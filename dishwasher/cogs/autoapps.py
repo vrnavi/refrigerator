b20ea840cad13983395a25e80eb7d81afe44e5b1
vrnavi <lapis@starrypoint.me>
2023-06-10 21:42:05 -0400

    fixes

--- dishwasher/cogs/autoapps.py
+++ dishwasher/cogs/autoapps.py
@@ -29,29 +29,31 @@ class AutoApps(Cog):
             and message.author.id
             == get_config(message.guild.id, "autoapp", "autoapp_id")
         ):
-            custom_msg = (
-                "\n" + get_config(message.guild.id, "autoapp", "autoapp_msg") + "\n"
-                if get_config(message.guild.id, "autoapp", "autoapp_msg")
-                else ""
-            )
+            custom_msg = None
             staledays = (
                 get_config(message.guild.id, "autoapp", "autoapp_staledays") * 86400
             )
-            staff_thread_name = (
+            threadname = (
                 user + "'s " + get_config(message.guild.id, "autoapp", "autoapp_name")
             )
+            staff_role = message.guild.get_role(
+                get_config(message.guild.id, "staff", "staff_role")
+            )
+            if staff_role:
+                minreq = int(len(staff_role.members) / 2 // 1 + 1)
             # Guild specific code.
-            if (
-                message.guild.id == 256926147827335170
-                and message.embeds[0].fields[2].value is not None
-            ):
-                user = await self.bot.fetch_user(message.embeds[0].fields[2].value[:-5])
+            if message.guild.id == 256926147827335170:
+                if message.embeds[0].fields[2].value is None:
+                    return
+                user = await self.bot.fetch_user(message.embeds[0].fields[2])
                 custom_msg = (
                     "\n"
-                    + f"Until it can be coded to automatically appear here, use `pws logs {message.embeds[0].fields[2].value}`.\nRemember to post ban context if available (ban record, modmail logs, etc.)."
+                    + f"Until it can be coded to automatically appear here, use `pws logs {user.id}`.\nRemember to post ban context if available (ban record, modmail logs, etc.)."
                     + "\n"
                 )
-            elif message.guild.id == 363821745590763520 and message.embeds is not None:
+            elif message.guild.id == 363821745590763520:
+                if message.embeds is None:
+                    return
                 user = await self.bot.fetch_user(message.content.split()[-1][:-1])
                 thread = await message.guild.get_channel(
                     1117253103700430868
@@ -65,6 +67,7 @@ class AutoApps(Cog):
                 await thread.send(
                     content=f"{user.mention}, this thread is for the discussion of your submitted character `{message.embeds[0].fields[2].value}` with the GMs."
                 )
+                minreq = 2
             else:
                 user = message.author
 
@@ -75,12 +78,16 @@ class AutoApps(Cog):
                 name=staff_thread_name,
                 reason=f"Automatic Applications by {self.bot.user.name}.",
             )
-            staff_role = message.guild.get_role(
-                get_config(message.guild.id, "staff", "staff_role")
-            )
-            await appthread.send(
-                content=f"Vote using reactions. Use this thread for discussion.\n`✅ = Yes`\n`❎ = No`\n`✳️ = Abstain`\n{custom_msg}\nThere are currently `{int(len(staff_role.members))}` Staff members at this time.\nVoting should end once one option reaches `{int(len(staff_role.members)/2//1+1)}` votes.\n\nThis appeal will turn stale on <t:{int(datetime.now(timezone.utc).timestamp())+staledays}:f>, or <t:{int(datetime.now(timezone.utc).timestamp())+staledays}:R>."
-            )
+            msg = f"Vote using reactions. Use this thread for discussion.\n`✅ = Yes`\n`❎ = No`\n`✳️ = Abstain`\n\n"
+            if custom_msg:
+                msg += custom_msg + "\n\n"
+            elif get_config(message.guild.id, "autoapp", "autoapp_msg"):
+                msg += get_config(message.guild.id, "autoapp", "autoapp_msg") + "\n\n"
+            if staff_role:
+                msg += f"There are currently `{int(len(staff_role.members))}` Staff members at this time.\nVoting should end once one option reaches `{minreq}` votes."
+            if staledays:
+                msg += f"\n\nThis appeal will turn stale on <t:{int(datetime.now(timezone.utc).timestamp())+staledays}:f>, or <t:{int(datetime.now(timezone.utc).timestamp())+staledays}:R>."
+            await appthread.send(content=msg)
 
 
 async def setup(bot):
