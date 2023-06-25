c27f9b32effeffa6598828010849b5235d8c5dfe
William M <cominatyou@cominatyou.com>
2023-06-25 16:35:49 -0500

    Port usertime and prefixes to voltage

--- dishwasher/cogs/prefixes.py
+++ dishwasher/cogs/prefixes.py
@@ -1,74 +1,62 @@
 import json
-import discord
 import config
-from discord.ext import commands
-from discord.ext.commands import Cog
+from voltage.ext import commands
+import voltage
 from helpers.userdata import get_userprefix, fill_userdata, set_userdata
 
 
-class prefixes(Cog):
+class prefixes(commands.SubclassedCog):
     """
-    Commands for letting users manage their custom prefixes, run command by itself to check prefixes.
+    Commands for letting users manage their custom prefixes.
     """
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(aliases=["prefix"], invoke_without_command=True)
+    @commands.command(aliases=["prefix"])
     async def prefixes(self, ctx):
         """[U] Lists all prefixes."""
-        embed = discord.Embed(
-            title="Your current prefixes...",
-            description="Mentioning the bot will always be a prefix.",
-            color=ctx.author.color,
-        )
-        embed.set_author(
-            icon_url=ctx.author.display_avatar.url, name=ctx.author.display_name
-        )
+        embed = voltage.embed.SendableEmbed(title="Your current prefixes...", description="")
+
         uid = str(ctx.author.id)
         userprefixes = get_userprefix(uid)
 
-        for i in range(
-            max(config.maxprefixes, len(userprefixes))
-        ):  # max of 24 prefixes as discord does not allow more than 25 fields in embeds
-            try:
-                value = userprefixes[i]
-            except (IndexError, TypeError):
-                value = "---"
-            finally:
-                embed.add_field(name=i + 1, value=f"{value}")
-        embed.set_footer(
-            text=f"Use {config.prefixes[0]}prefix add/remove to change your prefixes."
-        )
-        await ctx.reply(embed=embed, mention_author=False)
+        if (len(userprefixes)) == 0:
+            await ctx.reply("You do not have any prefixes set.", mention=False)
+            return
 
-    @prefixes.command()
-    async def add(self, ctx, *, arg: str):
-        """[U] Adds a new prefix."""
+        for i in range(min(config.maxprefixes, len(userprefixes))):
+            embed.description += f"**{i + 1}.** {userprefixes[i]}"
+            if (i + 1) < len(userprefixes):
+                embed.description += "\n"
+        await ctx.reply(embed=embed, mention=False)
+
+    @commands.command()
+    async def addprefix(self, ctx, *, arg: str):
         userdata, uid = fill_userdata(ctx.author.id)
         print(userdata)
         if not len(userdata[uid]["prefixes"]) >= config.maxprefixes:
             userdata[uid]["prefixes"].append(f"{arg} ")
             set_userdata(json.dumps(userdata))
-            await ctx.reply(content="Prefix added.", mention_author=False)
+            await ctx.reply(content="Prefix added.", mention=False)
         else:
             await ctx.reply(
                 content=f"You have reached your limit of {config.maxprefixes} prefixes.",
-                mention_author=False,
+                mention=False,
             )
 
-    @prefixes.command()
-    async def remove(self, ctx, number: int):
+    @commands.command()
+    async def removeprefix(self, ctx, number: int):
         """[U] Removes a prefix."""
         userdata, uid = fill_userdata(ctx.author.id)
         userdata[uid]["prefixes"]
         try:
             userdata[uid]["prefixes"].pop(number - 1)
             set_userdata(json.dumps(userdata))
-            await ctx.reply(content="Prefix removed.", mention_author=False)
+            await ctx.reply(content="Prefix removed.", mention=False)
         except IndexError:
-            await ctx.reply(content="This prefix does not exist.", mention_author=False)
+            await ctx.reply(content="This prefix does not exist.", mention=False)
 
 
-async def setup(bot):
-    await bot.add_cog(prefixes(bot))
+def setup(bot):
+    return prefixes(bot)
--- dishwasher/cogs/usertime.py
+++ dishwasher/cogs/usertime.py
@@ -1,13 +1,12 @@
 import json
-from discord import Member
 from datetime import datetime
 from zoneinfo import ZoneInfo, available_timezones
-from discord.ext.commands import Cog, Context, Bot
-from discord.ext import commands
+import voltage
+from voltage.ext import commands
 from helpers.userdata import fill_userdata, set_userdata
 
 
-class usertime(Cog):
+class usertime(commands.SubclassedCog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -26,24 +25,30 @@ class usertime(Cog):
                 "<https://en.wikipedia.org/wiki/List_of_tz_database_time_zones>\n"
                 "You can also use your GMT offset with the following format: `Etc/GMT<offset>`.\n"
                 "For example, `Etc/GMT+5` for Eastern Time, or UTC-5.",
-                mention_author=False,
+                mention=False,
             )
             return
         elif timezone not in available_timezones():
             await ctx.reply(
                 content="Invalid timezone provided. For help, run `timezone` by itself.",
-                mention_author=False,
+                mention=False,
             )
             return
         else:
             userdata[uid]["timezone"] = timezone
             set_userdata(json.dumps(userdata))
             await ctx.reply(
-                f"Your timezone has been set to `{timezone}`.", mention_author=False
+                f"Your timezone has been set to `{timezone}`.", mention=False
             )
 
     @commands.command(aliases=["tf"])
-    async def timefor(self, ctx, target: Member = None, *, time: str = None):
+    async def timefor(
+        self,
+        ctx,
+        target: voltage.Member = None,
+        *,
+        time: str = None,
+    ):
         """Send the current time in the invoker's (or mentioned user's) time zone."""
         if time and target.id != ctx.author.id:
             # check both *have* timezones
@@ -52,13 +57,13 @@ class usertime(Cog):
             if not suserdata[suid]["timezone"]:
                 await ctx.reply(
                     content="I have no idea what time it is for you. You can set your timezone with `timezone`.",
-                    mention_author=False,
+                    mention=False,
                 )
                 return
             elif not tuserdata[tuid]["timezone"]:
                 await ctx.reply(
                     content="I don't know what time it is for {target.display_name}.",
-                    mention_author=False,
+                    mention=False,
                 )
                 return
 
@@ -67,7 +72,7 @@ class usertime(Cog):
             if not parsed_time:
                 await ctx.reply(
                     content="Given time is invalid. Try `12AM`, `12 AM`, `12:00 AM`, or `00:00`.",
-                    mention_author=False,
+                    mention=False,
                 )
                 return
 
@@ -81,7 +86,7 @@ class usertime(Cog):
 
             await ctx.reply(
                 content=f"`{time}` for them is `{parsed_time.strftime('%I:%M %p')}` for you.",
-                mention_author=False,
+                mention=False,
             )
         else:
             userdata, uid = fill_userdata(ctx.author.id if not target else target.id)
@@ -92,13 +97,13 @@ class usertime(Cog):
                         if not target
                         else f"I don't know what time it is for {target.display_name}."
                     ),
-                    mention_author=False,
+                    mention=False,
                 )
                 return
             now = datetime.now(ZoneInfo(userdata[uid]["timezone"]))
             await ctx.reply(
                 content=f"{'Your' if not target else 'Their'} current time is `{now.strftime('%H:%M, %Y-%m-%d')}`",
-                mention_author=False,
+                mention=False,
             )
             return
 
@@ -111,5 +116,5 @@ class usertime(Cog):
         return None
 
 
-async def setup(bot: Bot):
-    await bot.add_cog(usertime(bot))
+def setup(bot):
+    return usertime(bot)
